d3327e2fbd9be411fe46e272dae97ce33524f37e
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-12 20:39:02 +0300

    fix environment parsing

--- .github/workflows/scripts/notifier.py
+++ .github/workflows/scripts/notifier.py
@@ -4,14 +4,22 @@ import os
 from pybrew import notification
 
 
-def make_message(environ):
-    where_str = f"{environ.get('WORKFLOW', 'Unknown workflow')} of {environ.get('REPOSITORY', 'unknown repository')}/{environ.get('BRANCH_NAME', 'unknown branch')}"
+def make_message(
+    FAILURE,
+    WORKFLOW,
+    REPOSITORY,
+    BRANCH_NAME,
+    EVENT_NAME,
+    HEAD_COMMIT_MESSAGE,
+    HEAD_COMMIT_URL
+):
+    where_str = f"{WORKFLOW} of {REPOSITORY}/{BRANCH_NAME}"
 
-    what_str = f"returned {'FAILURE ❌' if environ.get('FAILURE', True) else 'SUCCESS ✅'} on event {environ.get('EVENT_NAME', 'unknown event')}"
+    what_str = f"returned {'FAILURE ❌' if FAILURE else 'SUCCESS ✅'} on event {EVENT_NAME}"
 
     last_commit_str = (
-        f'Last commit was "{environ.get("HEAD_COMMIT_MESSAGE", "")}" {environ.get("HEAD_COMMIT_URL", "")}'
-        if 'HEAD_COMMIT_MESSAGE' in environ else
+        f'Last commit was "{HEAD_COMMIT_MESSAGE}" {HEAD_COMMIT_URL}'
+        if HEAD_COMMIT_MESSAGE else
         ''
     )
 
@@ -19,12 +27,17 @@ def make_message(environ):
 
 
 def main(environ):
-    environ['FAILURE'] = (
-        environ.get('FAILURE', 'true').lower().strip() == 'true'
-    )
     notification(
         channel='#website',
-        text=make_message(environ),
+        text=make_message(
+            FAILURE=environ.get('FAILURE', 'true').lower().strip() == 'true',
+            WORKFLOW=environ.get('WORKFLOW', 'Unknown workflow'),
+            REPOSITORY=environ.get('REPOSITORY', 'unknown repository'),
+            BRANCH_NAME=environ.get('BRANCH_NAME', 'unknown branch'),
+            EVENT_NAME=environ.get('EVENT_NAME', 'unknown event'),
+            HEAD_COMMIT_MESSAGE=environ.get('HEAD_COMMIT_MESSAGE', ''),
+            HEAD_COMMIT_URL=environ.get('HEAD_COMMIT_URL', '')
+        ),
         token=environ.get('SECRET_SLACK_BOT_TOKEN', '')
     )
 
