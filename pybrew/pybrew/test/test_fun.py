5d3d03a93391431762bff497e8474ca808de6cfb
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-12 17:12:12 +0300

    new notification function

--- pybrew/pybrew/test/test_fun.py
+++ pybrew/pybrew/test/test_fun.py
@@ -1,8 +1,14 @@
 import pytest
 from pybrew import *
 
+
 def test_my_fun():
     assert my_fun(4) == 5
 
-def test_notification():
-    assert notification(channel='#test', text='Hello World')
+
+def test_notification(SLACK_BOT_TOKEN):
+    assert notification(
+        channel='#test',
+        text='Hello World',
+        token=SLACK_BOT_TOKEN
+    )
