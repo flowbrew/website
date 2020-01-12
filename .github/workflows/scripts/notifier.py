662f3e9682e8aeb4d623437e989504e9ea1bc300
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-12 21:10:56 +0300

    minor format fix

--- .github/workflows/scripts/notifier.py
+++ .github/workflows/scripts/notifier.py
@@ -23,7 +23,7 @@ def make_message(
         ''
     )
 
-    return f'---\n{where_str} {what_str}.\n{last_commit_str}'
+    return f'{where_str} {what_str}.\n{last_commit_str}\n---'
 
 
 def main(environ):
