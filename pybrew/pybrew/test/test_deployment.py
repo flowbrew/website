994675a82a7f2362b86157e6e1e54dcb76e58486
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-29 17:05:37 +0300

    fix

--- pybrew/pybrew/test/test_deployment.py
+++ pybrew/pybrew/test/test_deployment.py
@@ -253,10 +253,16 @@ def test_website_performance_io(URL):
         ):
             if name == 'uses-long-cache-ttl':
                 assert audit['score'] >= 0.3
+
             elif is_mobile and name == 'first-contentful-paint-3g':
                 assert audit['score'] >= 0.5
+
+            elif is_mobile and name == 'first-cpu-idle':
+                assert audit['score'] >= 0.5
+
             elif is_mobile and name == 'interactive':
                 assert audit['score'] >= 0.6
+
             else:
                 assert audit['score'] >= 0.75
 
