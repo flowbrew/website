42ca7cfbbb032f8e0d776c50b4b8e36a981b8041
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-29 18:07:18 +0300

    ned gtm test

--- pybrew/pybrew/test/test_deployment.py
+++ pybrew/pybrew/test/test_deployment.py
@@ -254,14 +254,14 @@ def test_website_performance_io(URL):
             if name == 'uses-long-cache-ttl':
                 assert audit['score'] >= 0.3
 
-            elif is_mobile and name == 'first-contentful-paint-3g':
-                assert audit['score'] >= 0.5
+            # elif is_mobile and name == 'first-contentful-paint-3g':
+            #     assert audit['score'] >= 0.5
 
-            elif is_mobile and name == 'first-cpu-idle':
-                assert audit['score'] >= 0.4
+            # elif is_mobile and name == 'first-cpu-idle':
+            #     assert audit['score'] >= 0.4
 
-            elif is_mobile and name == 'interactive':
-                assert audit['score'] >= 0.5
+            # elif is_mobile and name == 'interactive':
+            #     assert audit['score'] >= 0.5
 
             else:
                 assert audit['score'] >= 0.75
