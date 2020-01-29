5a846b723d5877a3634293b139a2916fba7ba3dd
Aleksey Kozin <cirnotoss@gmail.com>
2020-01-29 15:04:02 +0300

    interactive budget for mobile lowered

--- pybrew/pybrew/test/test_deployment.py
+++ pybrew/pybrew/test/test_deployment.py
@@ -253,6 +253,8 @@ def test_website_performance_io(URL):
         ):
             if name == 'uses-long-cache-ttl':
                 assert audit['score'] >= 0.3
+            if is_mobile and name == 'interactive':
+                assert audit['score'] >= 0.6
             else:
                 assert audit['score'] >= 0.75
 
